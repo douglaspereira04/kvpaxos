8a32626ffd39337848487300a7b40ef88e74a4a6
Douglas Pereira Luiz <douglaspereira04@hotmail.com>
2023-10-05 09:27:50 -0300

    Removes careless from compiled versions

--- compile_all.sh
+++ compile_all.sh
@@ -6,8 +6,5 @@ mv ./build/bin/replica ./build/bin/old
 ./compile.sh TBB FREE OLD_GRAPH
 mv ./build/bin/replica ./build/bin/free
 
-./compile.sh TBB CARELESS TBB_GRAPH
-mv ./build/bin/replica ./build/bin/careless
-
 ./compile.sh TBB NON_STOP OLD_GRAPH
 mv ./build/bin/replica ./build/bin/non_stop
