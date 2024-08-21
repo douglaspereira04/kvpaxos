c5f6c8a76af4141057762cae6011a739ff60fd1d
Douglas Pereira Luiz <douglaspereira04@hotmail.com>
2024-08-21 16:08:47 -0300

    Adds async experiments

--- experiments/run.sh
+++ experiments/run.sh
@@ -6,38 +6,50 @@ node=$(get_node_id)
 case $node in
 
   0)
-    ./non_stop_ycsb_a.sh
+    ./async_ycsb_a.sh
     ;;
 
   1)
-    ./non_stop_ycsb_d.sh
+    ./async_ycsb_d.sh
     ;;
 
   2)
-    ./non_stop_ycsb_e.sh
+    ./async_ycsb_e.sh
     ;;
 
   3)
-    ./old_ycsb_a.sh
+    ./non_stop_ycsb_a.sh
     ;;
 
   4)
-    ./old_ycsb_d.sh
+    ./non_stop_ycsb_d.sh
     ;;
 
   5)
-    ./old_ycsb_e.sh
+    ./non_stop_ycsb_e.sh
     ;;
 
   6)
-    ./rr_ycsb_a.sh
+    ./old_ycsb_a.sh
     ;;
 
   7)
-    ./rr_ycsb_d.sh
+    ./old_ycsb_d.sh
     ;;
 
   8)
+    ./old_ycsb_e.sh
+    ;;
+
+  9)
+    ./rr_ycsb_a.sh
+    ;;
+
+  10)
+    ./rr_ycsb_d.sh
+    ;;
+
+  11)
     ./rr_ycsb_e.sh
     ;;
 
