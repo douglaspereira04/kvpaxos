2a609486df86cb588342bfb446a6a68b8f054ed4
Douglas Pereira Luiz <douglaspereira04@hotmail.com>
2024-08-27 09:05:00 -0300

    Comments cpu affinity

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -5,9 +5,9 @@
 namespace utils{ 
 
 void set_affinity(size_t cpu, std::thread &thread, cpu_set_t &cpu_set){
-	CPU_ZERO(&cpu_set);
-	CPU_SET(cpu, &cpu_set);
-	assert(pthread_setaffinity_np(thread.native_handle(), sizeof(cpu_set_t), &cpu_set) == 0);
+	//CPU_ZERO(&cpu_set);
+	//CPU_SET(cpu, &cpu_set);
+	//assert(pthread_setaffinity_np(thread.native_handle(), sizeof(cpu_set_t), &cpu_set) == 0);
 }
 
 }
