b09d446fc1e2ae172f60354b44d8477f494a926d
Douglas Pereira Luiz <douglaspereira04@hotmail.com>
2024-09-06 10:33:42 -0300

    Revert "Changes experiments to fit d820"
    This reverts commit bd896b075405095ee7911cb57b47f445e24156d2.

--- experiments/batch_ycsb_e.sh
+++ experiments/batch_ycsb_e.sh
@@ -2,13 +2,13 @@
 source ./experiments.sh
 
 _methods=(METIS)
-_partitions=(16)
+_partitions=(8)
 _versions=(batch)
 _workloads=(ycsb_e)
 _n_initial_keys=(1000000)
 _parameters_file="batch_ycsb_e_parameters.txt"
 _reps=1
-_arrival_rates=(0)
+_arrival_rates=(15122 36115 57108)
 _arrival_rate_seed=1672270886
 
 experiments _methods _partitions _versions _workloads _n_initial_keys _arrival_rates $_arrival_rate_seed $_parameters_file $_reps
--- experiments/old_ycsb_d.sh
+++ experiments/old_ycsb_d.sh
@@ -2,13 +2,13 @@
 source ./experiments.sh
 
 _methods=(METIS)
-_partitions=(16)
+_partitions=(8)
 _versions=(old)
 _workloads=(ycsb_d)
 _n_initial_keys=(1000000)
 _parameters_file="old_ycsb_a_d_parameters.txt"
 _reps=1
-_arrival_rates=(0)
+_arrival_rates=(635222 664214 693206)
 _arrival_rate_seed=1672270886
 
 experiments _methods _partitions _versions _workloads _n_initial_keys _arrival_rates $_arrival_rate_seed $_parameters_file $_reps
