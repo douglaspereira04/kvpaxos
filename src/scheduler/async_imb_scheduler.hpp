e85bab7358eb213e64bc4a0df82c3962c50f5207
Douglas Pereira Luiz <douglaspereira04@hotmail.com>
2025-04-01 10:18:40 -0300

    Changes to imb old 10

--- src/scheduler/async_imb_scheduler.hpp
+++ src/scheduler/async_imb_scheduler.hpp
@@ -131,7 +131,7 @@ public:
                     break;
                 }
             } else {
-                this->sucessive_imbalance_[i] = 0;//this->sucessive_imbalance_[i] - (this->sucessive_imbalance_[i] > 0); // (this->sucessive_imbalance_[i] >> 1) | 0b1;
+                this->sucessive_imbalance_[i] = this->sucessive_imbalance_[i] - (this->sucessive_imbalance_[i] > 0); // (this->sucessive_imbalance_[i] >> 1) | 0b1;
             }
         }
 
