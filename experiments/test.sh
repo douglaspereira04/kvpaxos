a0783f6a3dadeac45db6a3ce5f1b292a639a347d
Douglas Pereira Luiz <douglaspereira04@hotmail.com>
2025-06-12 23:47:19 -0300

    adds snapshot scan option to testscrip

--- experiments/test.sh
+++ experiments/test.sh
@@ -5,21 +5,23 @@
 
 n_initial_keys=1000000
 arrival_rate_seed=1672270886
-snapsho_scan=0
+snapshop_scan=(0 1)
 workloads=(ycsb_a ycsb_d ycsb_e)
 requests=(50000000 50000000 5000000)
 
 mkdir -p output
-for ((i=0; i<${#workloads[@]}; i++)); do
-    file_name=single_w${workloads[$i]}.csv
-    echo ./single ${requests[$i]} $n_initial_keys  ${workloads[$i]}_requests.txt  0  $arrival_rate_seed $snapsho_scan
-    ./single ${requests[$i]} $n_initial_keys  ${workloads[$i]}_requests.txt  0  $arrival_rate_seed $snapsho_scan > output/$file_name
-    if [ $? -ne 0 ]; then
-        echo "ERROR"
-        break
-    fi
-    mv details.csv output/details_$file_name
-    rm partition_output_*
+for snapshop_scan_ in "${snapshop_scan[@]}"; do
+    for ((i=0; i<${#workloads[@]}; i++)); do
+        file_name=single_${snapshop_scan_}_w${workloads[$i]}.csv
+        echo ./single ${requests[$i]} $n_initial_keys  ${workloads[$i]}_requests.txt  0  $arrival_rate_seed $snapshop_scan_
+        ./single ${requests[$i]} $n_initial_keys  ${workloads[$i]}_requests.txt  0  $arrival_rate_seed $snapshop_scan_ > output/$file_name
+        if [ $? -ne 0 ]; then
+            echo "ERROR"
+            break
+        fi
+        mv details.csv output/details_$file_name
+        rm partition_output_*
+    done;
 done;
 mkdir -p $1
 cp -r output $1
