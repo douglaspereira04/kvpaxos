c2961417bccb28ceceb5ca3fc8d15c0f49998889
Douglas Pereira Luiz <douglasp@node0.douglasp-260426.scalablesmr.emulab.net>
2025-06-14 20:29:21 +0000

    changes config

--- compile_all.sh
+++ compile_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #./async_imb_0_0 configs/config.toml 2 100000 1000000 METIS ycsb_a_100000_requests.txt 0 167227088 1000 0.05
-track_length=(0 100000)
-q_size=(0 100000)
+track_length=(0 1000000)
+q_size=(0 10000)
 
 
 for q_size_ in "${q_size[@]}"; do
