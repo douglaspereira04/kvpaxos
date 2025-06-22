f4aa7728d863e803c1c394ec69c7bd9f66a7be14
Douglas Pereira Luiz <douglaspereira04@hotmail.com>
2025-06-22 10:46:04 -0300

    Adds engines to compile all

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@ git pull --recurse-submodules
 git submodule update --init --recursive
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. -DINFO=ON -DLINEARIZABLE=ON -DEDGES=OFF -DENGINE=TKRZW -DANSWER=OFF -DREPARTITIONING=$1 -DQ_SIZE=$2
+cmake -DCMAKE_BUILD_TYPE=Release .. -DINFO=ON -DLINEARIZABLE=ON -DEDGES=OFF -DENGINE=$1 -DANSWER=OFF -DREPARTITIONING=$2 -DQ_SIZE=$3
 make -j$(nproc)
--- compile_all.sh
+++ compile_all.sh
@@ -2,15 +2,19 @@
 
 #./async_imb_0_0 configs/config.toml 2 100000 1000000 METIS ycsb_a_100000_requests.txt 0 167227088 1000 0.05
 q_size=(0 100000)
-
+engines=(TKRZW LMDB LEVEL_DB)
 
 mkdir -p build/bin
-for q_size_ in "${q_size[@]}"; do
-    ./compile.sh ON ${q_size_}
-    mv ./build/src/main ./build/bin/rep_${q_size_}
+for e_ in "${engines[@]}"; do
+    for q_size_ in "${q_size[@]}"; do
+        ./compile.sh ${e_} ON ${q_size_}
+        mv ./build/src/main ./build/bin/rep_${e_}_${q_size_}
+    done;
 done;
 
-./compile.sh OFF 0 0
-mv ./build/src/main ./build/bin/stat
+for e_ in "${engines[@]}"; do
+    ./compile.sh ${e_} OFF 0
+    mv ./build/src/main ./build/bin/stat_${e_}
+done;
 
 cp -r experiments/test.sh build/bin/
