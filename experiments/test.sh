f465f879c0c7ffb42cf9d648b42da47ba05dc6e7
Douglas Pereira Luiz <douglaspereira04@hotmail.com>
2025-06-22 14:15:12 -0300

    Updates test scripts

--- experiments/test.sh
+++ experiments/test.sh
@@ -3,16 +3,16 @@
 #                    #requests  #initial keys    request file       #rate mean   #rate seed
 #./single_100000      1000000      100000      ycsb_a_requests.txt     0        167227088
 
-n_initial_keys=1000000
 arrival_rate_seed=1672270886
 workloads=(ycsb_a ycsb_d ycsb_e)
-requests=(10000000 10000000 1000000)
+n_initial_keys=(1000000 1000000 100000)
+requests=(10000000 10000000 100000)
 
 mkdir -p output
 for ((i=0; i<${#workloads[@]}; i++)); do
     file_name=single_w${workloads[$i]}.csv
-    echo ./single ${requests[$i]} $n_initial_keys  ${workloads[$i]}_requests.txt  0  $arrival_rate_seed
-    ./single ${requests[$i]} $n_initial_keys  ${workloads[$i]}_requests.txt  0  $arrival_rate_seed > output/$file_name
+    echo ./single ${requests[$i]} ${n_initial_keys[$i]}  ${workloads[$i]}_requests.txt  0  $arrival_rate_seed
+    ./single ${requests[$i]} ${n_initial_keys[$i]}  ${workloads[$i]}_requests.txt  0  $arrival_rate_seed > output/$file_name
     if [ $? -ne 0 ]; then
         echo "ERROR"
         break
